6c84932114821aec56588391b6cc387bd2d11b78
Zoe Afshar <zoe.afshar@gmail.com>
2015-12-31 08:50:13 -0500

    changed "dependencies" to peas and "dep" to pod

--- src/main/scala/dependency/Task.scala
+++ src/main/scala/dependency/Task.scala
@@ -10,34 +10,34 @@ import scala.reflect.ClassTag
 abstract class Task [T: ClassTag](implicit p: Peapod) {
   lazy val name: String = this.getClass.getName
   protected val version: String = "1"
-  protected val pods= scala.collection.mutable.ArrayBuffer.empty[Task[_]]
+  protected val peas= scala.collection.mutable.ArrayBuffer.empty[Task[_]]
 
   protected lazy val dir = p.fs + p.path + "/" + name + "/" + recursiveVersionShort
 
   def get(): T = {
     //Adds all dependencies to workflow
-    pods.foreach(d => p.put(this, d))
+    peas.foreach(d => p.put(this, d))
     //Builds all dependencies
     if(! exists()) {
-      pods.foreach(d => p.build(d))
+      peas.foreach(d => p.build(d))
     }
     val f= p.build(this)
     val t= Await.result(f, Duration.Inf).asInstanceOf[T]
     //Removes dependencies from workflow cache is not needed, this allows them to be unpersisted automatically
-    pods.foreach(d => p.removeIfUnneeded(d.name))
+    peas.foreach(d => p.removeIfUnneeded(d.name))
     p.removeIfUnneeded(this.name)
     t
   }
 
   protected[dependency] def build(): T
 
-  protected def dep[D <: Task[_]](d: D): D = {
-    pods += d
+  protected def pod[D <: Task[_]](d: D): D = {
+    peas += d
     d
   }
 
   def recursiveVersion: List[String] = {
-    name + ":" + version :: pods.flatMap(_.recursiveVersion.map("-" + _)).toList
+    name + ":" + version :: peas.flatMap(_.recursiveVersion.map("-" + _)).toList
   }
   def recursiveVersionShort: String = {
     val bytes = MD5Hash.digest(recursiveVersion.mkString("\n")).getDigest
