719010552ccb8a31c08a1421333ef6edda95d713
Marcin Mejran <mejran@gmail.com>
2016-06-16 13:14:59 -0700

    Added unit tests for Implicits class

---
+++ src/test/scala/peapod/ImplicitsTest.scala
@@ -0,0 +1,17 @@
+package peapod
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.scalatest.FunSuite
+
+class ImplicitsTest extends FunSuite{
+
+  import Implicits._
+
+  test("testConfig") {
+    val conf = ConfigFactory.empty()
+      .set("test", 42)
+      .set("test2", "test")
+    assert(conf.getInt("test")== 42)
+    assert(conf.getString("test2")== "test")
+  }
+}
