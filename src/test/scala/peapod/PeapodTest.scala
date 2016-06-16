a89a7a48a798ab8be9e5dc201968f047a377c0c1
Marcin Mejran <mejran@gmail.com>
2016-06-16 13:15:23 -0700

    Added and refactored unit tests for Peapod class

--- src/test/scala/peapod/PeapodTest.scala
+++ src/test/scala/peapod/PeapodTest.scala
@@ -1,164 +1,53 @@
 package peapod
 
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Date
-
-import com.google.common.io.Resources
-import org.apache.commons.codec.binary.Base64
-import org.apache.commons.codec.net.URLCodec
-import org.apache.spark.ml.PipelineModel
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
-import org.apache.spark.ml.feature.{HashingTF, StopWordsRemover, Tokenizer}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.DataFrame
-import org.joda.time.LocalDate
-import org.scalatest.FunSuite
-import StorableTask._
-import Pea._
 import generic.PeapodGenerator
-import org.apache.hadoop.fs.Path
-
-import scala.util.Random
+import org.scalatest.FunSuite
+import peapod.PeapodTest.{TaskA, TaskB}
 
 object PeapodTest {
-  var runs = 0
-
-  def upRuns() = this.synchronized {
-    runs+=1
+  class TaskA(implicit val p: Peapod) extends EphemeralTask[Double]  {
+    def generate = 1
   }
 
-  case class DependencyInput(label: Double, text: String)
-
-  class Raw(implicit val p: Peapod) extends StorableTask[RDD[DependencyInput]]
-     {
-    override val version = "2"
-    override val description = "Loading data from dependency.csv"
-    def generate = {
-      upRuns()
-      p.sc.textFile("file://" + Resources.getResource("dependency.csv").getPath)
-        .map(_.split(","))
-        .map(l => new DependencyInput(l(0).toDouble, l(1)))
-    }
-  }
-
-  class Parsed(implicit val p: Peapod) extends StorableTask[DataFrame] {
-    import p.sqlCtx.implicits._
-    val raw = pea(new Raw)
-    def generate = {
-      upRuns()
-      raw().toDF()
-    }
-  }
-
-  class ParsedEphemeral(implicit val p: Peapod) extends EphemeralTask[DataFrame] {
-    import p.sqlCtx.implicits._
-    val raw = pea(new Raw)
-    def generate = {
-      upRuns()
-      raw().toDF()
-    }
-  }
-
-  class PipelineFeature(implicit val p: Peapod) extends StorableTask[PipelineModel] {
-    val parsed = pea(new Parsed)
-    override val version = "2"
-    def generate = {
-      upRuns()
-      val training = parsed.get()
-      val tokenizer = new Tokenizer()
-        .setInputCol("text")
-        .setOutputCol("TextTokenRaw")
-      val remover = new (StopWordsRemover)
-        .setInputCol(tokenizer.getOutputCol)
-        .setOutputCol("TextToken")
-      val hashingTF = new HashingTF()
-        .setNumFeatures(5)
-        .setInputCol(remover.getOutputCol)
-        .setOutputCol("features")
-
-      val pipeline = new org.apache.spark.ml.Pipeline()
-        .setStages(Array(tokenizer,remover, hashingTF))
-      pipeline.fit(training)
-    }
-  }
-
-  class PipelineLR(implicit val p: Peapod) extends StorableTask[PipelineModel] {
-    val pipelineFeature = pea(new PipelineFeature())
-    val parsed = pea(new Parsed)
-    def generate = {
-      upRuns()
-      val training = parsed
-      val lr = new LogisticRegression()
-        .setMaxIter(25)
-        .setRegParam(0.01)
-        .setFeaturesCol("features")
-        .setLabelCol("label")
-      val pipeline = new org.apache.spark.ml.Pipeline()
-        .setStages(Array(lr))
-      pipeline.fit(pipelineFeature.get().transform(training.get()))
-    }
-  }
-
-  class AUC(implicit val p: Peapod) extends StorableTask[Double]  {
-    override val description = "AUC generated for a model"
-    val pipelineLR = pea(new PipelineLR())
-    val pipelineFeature = pea(new PipelineFeature())
-    val parsed = pea(new Parsed)
-    def generate = {
-      upRuns()
-      val training = parsed.get()
-      val transformed = pipelineFeature.get().transform(training)
-      val predictions = pipelineLR.get().transform(transformed)
-      val evaluator = new BinaryClassificationEvaluator()
-      evaluator.evaluate(predictions)
-    }
+  class TaskB(implicit val p: Peapod) extends EphemeralTask[Double]  {
+    pea(new TaskA())
+    def generate = 1
   }
 
 }
-
-class PeapodTest extends FunSuite {
-  test("testRunWorkflowConcurrentCache") {
-    implicit val w = PeapodGenerator.peapod()
-    PeapodTest.runs = 0
-    (1 to 10).par.foreach{i => w.pea(new PeapodTest.AUC()).get()}
-    assert(PeapodTest.runs == 5)
-    Thread.sleep(1000)
-    PeapodTest.runs = 0
-    (1 to 10).par.foreach{i => w.pea(new PeapodTest.AUC()).get()}
-    assert(PeapodTest.runs == 0)
-  }
-  test("testRunWorkflow") {
-    implicit val w = PeapodGenerator.peapod()
-    PeapodTest.runs = 0
-    w(new PeapodTest.PipelineFeature()).get()
-    w(new PeapodTest.ParsedEphemeral())
-    w(new PeapodTest.AUC())
-    println(w.dotFormatDiagram())
-    println(Util.gravizoDotLink(w.dotFormatDiagram()))
-    println(Util.teachingmachinesDotLink(w.dotFormatDiagram()))
-
-    println(w.pea(new PeapodTest.AUC()).get())
-    assert(PeapodTest.runs == 5)
-    PeapodTest.runs = 0
-    println(w.pea(new PeapodTest.AUC()).get())
-    assert(PeapodTest.runs == 0)
-    PeapodTest.runs = 0
-    println(w.pea(new PeapodTest.ParsedEphemeral()).get())
-    assert(PeapodTest.runs == 1)
-    println("http://g.gravizo.com/g?" +
-      new URLCodec().encode(w.dotFormatDiagram()).replace("+","%20"))
-    assert(w.size() == 6)
-
+class PeapodTest  extends FunSuite {
+  test("testDependencies") {
+    implicit val p = PeapodGenerator.peapod()
+    val peaA = p(new TaskA())
+    val peaB = p(new TaskB())
+    assert(peaA.parents == Set(peaB))
+    assert(peaB.children == Set(peaA))
   }
 
-  test("testRunWorkflowConcurrent") {
-    implicit val w = PeapodGenerator.peapod()
-    PeapodTest.runs = 0
-    (1 to 10).par.foreach{i => w.pea(new PeapodTest.AUC()).get()}
-    assert(PeapodTest.runs == 5)
+  test("testSize") {
+    implicit val p = PeapodGenerator.peapod()
+    val peaA = p(new TaskA())
+    assert(p.size() == 1)
+    val peaB = p(new TaskB())
+    assert(p.size() == 2)
   }
 
+  test("testDotFormatter") {
+    implicit val p = PeapodGenerator.peapod()
+    p(new TaskB())
+    val dot = p.dotFormatDiagram()
+    assert(dot == "digraph G {node [shape=box]\"peapod.PeapodTest$TaskB\" [style=dotted];\n" +
+      "\"peapod.PeapodTest$TaskA\" [style=dotted];\"peapod.PeapodTest$TaskA\"->\"peapod.PeapodTest$TaskB\"" +
+      ";{ rank=same;\"peapod.PeapodTest$TaskB\"}{ rank=same;\"peapod.PeapodTest$TaskA\"}}")
 
+    assert(Util.gravizoDotLink(dot) == "http://g.gravizo.com/g?digraph%20G%20%7Bnode%20%5Bshape%3Dbox%5D%22" +
+      "peapod.PeapodTest%24TaskB%22%20%5Bstyle%3Ddotted%5D%3B%0A%22peapod.PeapodTest%24TaskA%22%20%5Bstyle%3" +
+      "Ddotted%5D%3B%22peapod.PeapodTest%24TaskA%22-%3E%22peapod.PeapodTest%24TaskB%22%3B%7B%20rank%3Dsame%3B%22" +
+      "peapod.PeapodTest%24TaskB%22%7D%7B%20rank%3Dsame%3B%22peapod.PeapodTest%24TaskA%22%7D%7D")
+
+    assert(Util.teachingmachinesDotLink(dot) == "http://graphvizserver-env.elasticbeanstalk.com/?H4sIAAAAAAAAAEv" +
+      "JTC9KLMhQcFeozstPSVWILs5ILEi1TcqviFUqSE0syE_RCwBTIanFJSohicXZTkpARSWVOam2KfklJakpsdZcOFQ6YqjEqVDXDqdt1tU" +
+      "KRYl52bbFibmpuAxwUqolQpWjUm0tAJr9mSDwAAAA")
+
+  }
 }
---
+++ src/test/scala/peapod/PeapodWorkflowTest.scala
@@ -0,0 +1,165 @@
+package peapod
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.google.common.io.Resources
+import org.apache.commons.codec.binary.Base64
+import org.apache.commons.codec.net.URLCodec
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{HashingTF, StopWordsRemover, Tokenizer}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.DataFrame
+import org.joda.time.LocalDate
+import org.scalatest.FunSuite
+import StorableTask._
+import Pea._
+import generic.PeapodGenerator
+import org.apache.hadoop.fs.Path
+
+import scala.util.Random
+
+object PeapodWorkflowTest {
+  var runs = 0
+
+  def upRuns() = this.synchronized {
+    runs+=1
+  }
+
+  case class DependencyInput(label: Double, text: String)
+
+  class Raw(implicit val p: Peapod) extends StorableTask[RDD[DependencyInput]]
+     {
+    override val version = "2"
+    override val description = "Loading data from dependency.csv"
+    def generate = {
+      upRuns()
+      p.sc.textFile("file://" + Resources.getResource("dependency.csv").getPath)
+        .map(_.split(","))
+        .map(l => new DependencyInput(l(0).toDouble, l(1)))
+    }
+  }
+
+  class Parsed(implicit val p: Peapod) extends StorableTask[DataFrame] {
+    import p.sqlCtx.implicits._
+    val raw = pea(new Raw)
+    def generate = {
+      upRuns()
+      raw().toDF()
+    }
+  }
+
+  class ParsedEphemeral(implicit val p: Peapod) extends EphemeralTask[DataFrame] {
+    import p.sqlCtx.implicits._
+    val raw = pea(new Raw)
+    def generate = {
+      upRuns()
+      raw().toDF()
+    }
+  }
+
+  class PipelineFeature(implicit val p: Peapod) extends StorableTask[PipelineModel] {
+    val parsed = pea(new Parsed)
+    override val version = "2"
+    def generate = {
+      upRuns()
+      val training = parsed.get()
+      val tokenizer = new Tokenizer()
+        .setInputCol("text")
+        .setOutputCol("TextTokenRaw")
+      val remover = new (StopWordsRemover)
+        .setInputCol(tokenizer.getOutputCol)
+        .setOutputCol("TextToken")
+      val hashingTF = new HashingTF()
+        .setNumFeatures(5)
+        .setInputCol(remover.getOutputCol)
+        .setOutputCol("features")
+
+      val pipeline = new org.apache.spark.ml.Pipeline()
+        .setStages(Array(tokenizer,remover, hashingTF))
+      pipeline.fit(training)
+    }
+  }
+
+  class PipelineLR(implicit val p: Peapod) extends StorableTask[PipelineModel] {
+    val pipelineFeature = pea(new PipelineFeature())
+    val parsed = pea(new Parsed)
+    def generate = {
+      upRuns()
+      val training = parsed
+      val lr = new LogisticRegression()
+        .setMaxIter(25)
+        .setRegParam(0.01)
+        .setFeaturesCol("features")
+        .setLabelCol("label")
+      val pipeline = new org.apache.spark.ml.Pipeline()
+        .setStages(Array(lr))
+      pipeline.fit(pipelineFeature.get().transform(training.get()))
+    }
+  }
+
+  class AUC(implicit val p: Peapod) extends StorableTask[Double]  {
+    override val description = "AUC generated for a model"
+    val pipelineLR = pea(new PipelineLR())
+    val pipelineFeature = pea(new PipelineFeature())
+    val parsed = pea(new Parsed)
+    def generate = {
+      upRuns()
+      val training = parsed.get()
+      val transformed = pipelineFeature.get().transform(training)
+      val predictions = pipelineLR.get().transform(transformed)
+      val evaluator = new BinaryClassificationEvaluator()
+      evaluator.evaluate(predictions)
+    }
+  }
+
+}
+
+class PeapodWorkflowTest extends FunSuite {
+  test("testRunWorkflowConcurrentCache") {
+    implicit val w = PeapodGenerator.peapod()
+    PeapodWorkflowTest.runs = 0
+    (1 to 10).par.foreach{i => w.pea(new PeapodWorkflowTest.AUC()).get()}
+    assert(PeapodWorkflowTest.runs == 5)
+    Thread.sleep(1000)
+    PeapodWorkflowTest.runs = 0
+    (1 to 10).par.foreach{i => w.pea(new PeapodWorkflowTest.AUC()).get()}
+    assert(PeapodWorkflowTest.runs == 0)
+  }
+  test("testRunWorkflow") {
+    implicit val w = PeapodGenerator.peapod()
+    PeapodWorkflowTest.runs = 0
+    w(new PeapodWorkflowTest.PipelineFeature()).get()
+    w(new PeapodWorkflowTest.ParsedEphemeral())
+    w(new PeapodWorkflowTest.AUC())
+
+    w.pea(new PeapodWorkflowTest.AUC()).get()
+
+    assert(PeapodWorkflowTest.runs == 5)
+
+    PeapodWorkflowTest.runs = 0
+    w.pea(new PeapodWorkflowTest.AUC()).get()
+
+    assert(PeapodWorkflowTest.runs == 0)
+
+    PeapodWorkflowTest.runs = 0
+
+    w.pea(new PeapodWorkflowTest.ParsedEphemeral()).get()
+
+    assert(PeapodWorkflowTest.runs == 1)
+    assert(w.size() == 6)
+
+  }
+
+  test("testRunWorkflowConcurrent") {
+    implicit val w = PeapodGenerator.peapod()
+    PeapodWorkflowTest.runs = 0
+    (1 to 10).par.foreach{i => w.pea(new PeapodWorkflowTest.AUC()).get()}
+    assert(PeapodWorkflowTest.runs == 5)
+  }
+
+
+}
