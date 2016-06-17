b28bb94ecc22a35514a400f2a22d366455888bd4
Marcin Mejran <mejran@gmail.com>
2016-06-17 14:58:33 -0700

    Set the WrappedTask as private as it's not expected to be extended outside of the Peapod classes

--- src/main/scala/peapod/WrappedTask.scala
+++ src/main/scala/peapod/WrappedTask.scala
@@ -5,7 +5,7 @@ import scala.reflect.ClassTag
 /**
   * A Task that can be automatically cached in a Peapod object, used internally for dependencies
   */
-class WrappedTask[+T: ClassTag] (val p: Peapod, val t: Task[T]) {
+private class WrappedTask[+T: ClassTag] (val p: Peapod, val t: Task[T]) {
   def get() : T = {
     p(t).get()
   }
