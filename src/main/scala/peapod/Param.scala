3ae5d43aa7d933e9ad217564d88b691417e42362
Marcin Mejran <mejran@gmail.com>
2016-06-18 20:56:48 -0700

    Support for parametrized tasks

--- src/main/scala/peapod/Param.scala
+++ src/main/scala/peapod/Param.scala
@@ -1,6 +1,22 @@
 package peapod
 
 /**
+  * Provides support for parametrized Tasks, each parameter is created as an instance of Param and then Tasks inherit
+  * those classes. This class ensures that the naming of Task's is unique for each combination of parameters.
+  * For example:
+  *
+  *   trait ParamA extends Param {
+  *     val a: String
+  *     param(a)
+  *   }
+  *   trait ParamB extends Param {
+  *     val b: String
+  *     param(b)
+  *   }
+  *   class Test(val a: String, val b: String)(implicit val p: Peapod)
+  *     extends EphemeralTask[Double] with ParamA with ParamB {
+  *     def generate = 1
+  *   }
   *
   */
 trait Param extends Task[Any] {
