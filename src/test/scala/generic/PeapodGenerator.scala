7ebc52e67957f041d44f0e0f894642104b9e3d44
Marcin Mejran <mejran@gmail.com>
2016-06-19 12:28:49 -0700

    Automatically remove temporary directories when the JVM exits

--- src/test/scala/generic/PeapodGenerator.scala
+++ src/test/scala/generic/PeapodGenerator.scala
@@ -13,21 +13,24 @@ import scala.util.Random
   * Created by Marcin on 6/15/2016.
   */
 object PeapodGenerator {
-  def peapod() = {
+  def createTempDir(): String = {
     val sdf = new SimpleDateFormat("ddMMyy-hhmmss")
     val path = System.getProperty("java.io.tmpdir") + "workflow-" + sdf.format(new Date()) + Random.nextInt()
     new File(path).mkdir()
     new File(path).deleteOnExit()
+    ShutdownHookManager.registerShutdownDeleteDir(new File(path))
+    path
+  }
+
+  def peapod() = {
+    val path = createTempDir()
     val w = new Peapod(
       path= new Path("file://",path.replace("\\","/")).toString,
       raw="")(generic.Spark.sc)
     w
   }
   def peapodNonRecursive() = {
-    val sdf = new SimpleDateFormat("ddMMyy-hhmmss")
-    val path = System.getProperty("java.io.tmpdir") + "workflow-" + sdf.format(new Date()) + Random.nextInt()
-    new File(path).mkdir()
-    new File(path).deleteOnExit()
+    val path = createTempDir()
     val w = new Peapod(
       path= new Path("file://",path.replace("\\","/")).toString,
       raw="")(generic.Spark.sc) {
