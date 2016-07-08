73eb394564c41db3127727442b3abbec52b1e82f
Marcin Mejran <marcin@mindfulmachines.io>
2016-07-07 19:36:53 -0700

    Switching to using Hadoop's deleteOnExit method versus using custom code

--- src/test/scala/generic/PeapodGenerator.scala
+++ src/test/scala/generic/PeapodGenerator.scala
@@ -1,10 +1,11 @@
 package generic
 
 import java.io.File
+import java.net.URI
 import java.text.SimpleDateFormat
 import java.util.Date
 
-import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.{FileSystem, Path}
 import peapod.{Peapod, ShutdownHookManager, Web}
 
 import scala.util.Random
@@ -15,24 +16,25 @@ import scala.util.Random
 object PeapodGenerator {
   def createTempDir(): String = {
     val sdf = new SimpleDateFormat("ddMMyy-hhmmss")
-    val path = System.getProperty("java.io.tmpdir") + "workflow-" + sdf.format(new Date()) + Random.nextInt()
-    new File(path).mkdir()
-    new File(path).deleteOnExit()
-    ShutdownHookManager.registerShutdownDeleteDir(new File(path))
+    val rawPath = System.getProperty("java.io.tmpdir") + "workflow-" + sdf.format(new Date()) + Random.nextInt()
+    val path = new Path("file://",rawPath.replace("\\","/")).toString
+    val fs = FileSystem.get(new URI(path), SparkS3.sc.hadoopConfiguration)
+    fs.mkdirs(new Path(path))
+    fs.deleteOnExit(new Path(path))
     path
   }
 
   def peapod() = {
     val path = createTempDir()
     val w = new Peapod(
-      path= new Path("file://",path.replace("\\","/")).toString,
+      path= path,
       raw="")(generic.Spark.sc)
     w
   }
   def peapodNonRecursive() = {
     val path = createTempDir()
     val w = new Peapod(
-      path= new Path("file://",path.replace("\\","/")).toString,
+      path= path,
       raw="")(generic.Spark.sc) {
       override val recursiveVersioning = false
     }
@@ -41,7 +43,7 @@ object PeapodGenerator {
   def web() = {
     val path = createTempDir()
     val w = new Peapod(
-      path= new Path("file://",path.replace("\\","/")).toString,
+      path= path,
       raw="")(generic.Spark.sc) with Web
     w
   }
